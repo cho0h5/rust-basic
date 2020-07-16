0bb7d595b78e70f0df47b196fb2213c2940a7bf3
Youngho Cho <dominoyh5@gmail.com>
2020-07-16 16:06:09 +0900

    Learning use glob

--- 07-module/importing_names/src/main.rs
+++ 07-module/importing_names/src/main.rs
@@ -1,13 +1,12 @@
-pub mod a {
-    pub mod series {
-        pub mod of {
-            pub fn nested_modules() {}
-        }
-    }
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
 }
 
-use a::series::of;
+use TrafficLight::*;
 
 fn main() {
-    of::nested_modules();
+    let red = Red;
+    let green = Green;
 }
