32343c119022a9380058d3361eeb5e18328f1e57
Youngho Cho <dominoyh5@gmail.com>
2020-07-16 17:19:56 +0900

    Learning entry

--- 08-collection/hashmap_basic/src/main.rs
+++ 08-collection/hashmap_basic/src/main.rs
@@ -1,15 +1,30 @@
 use std::collections::HashMap;
 
 fn main() {
-//    let mut scores = HashMap::new();
+//    //let field_name = String::from("Favorite color");
+//    let field_name = "Favorite color";
+//    let field_value = String::from("Blue");
 //
-//    scores.insert(String::from("Blue"), 10);
-//    scores.insert(String::from("Yellow"), 20);
+//    let mut map = HashMap::new();
+//    map.insert(field_name, field_value);
+//
+//    println!("{:?}", map);
 
-    let teams = vec![String::from("Blue"), String::from("Yellow")];
-    let initial_scores = vec![10, 50];
+    let mut scores = HashMap::new();
 
-    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
-    //let scores: Vec<_> = teams.iter().zip(initial_scores.iter()).collect();
-    println!("{:?}", scores);
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 100);
+
+//    let score = scores.get("Blue");
+//    println!("{}", score.unwrap());
+    
+    for (k, v) in &scores {
+        println!("{}: {}", k, v);
+    }
+
+    scores.entry("Yellow".to_string()).or_insert(5);
+    scores.entry("Blue".to_string()).or_insert(5);
+    scores.entry("Green".to_string()).or_insert(5);
+
+    println!("{:#?}", scores);
 }
