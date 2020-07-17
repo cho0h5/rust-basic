dbc99914ec98511ff7557568bb722ab8abde627c
Youngho Cho <dominoyh5@gmail.com>
2020-07-17 12:18:47 +0900

    Learning generic with struct and method

---
+++ 10-generic/generic_struct/src/main.rs
@@ -0,0 +1,16 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn main() {
+    let p = Point { x: 4, y: 0 };
+
+    println!("p.x = {}", p.x());
+}
