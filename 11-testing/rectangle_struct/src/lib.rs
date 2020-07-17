e30cee672bb16407edca78d3d3ba3cd90e1bda61
Youngho Cho <dominoyh5@gmail.com>
2020-07-17 16:38:41 +0900

    Learn assert

---
+++ 11-testing/rectangle_struct/src/lib.rs
@@ -0,0 +1,45 @@
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+
+    fn can_hold(&self, target: &Rectangle) -> bool {
+        self.length > target.length && self.width > target.width
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { length: size, width: size }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+}
+
+//fn main() {
+//    let square1 = Rectangle::square(30);
+//    println!("{:?}", square1);
+//}
