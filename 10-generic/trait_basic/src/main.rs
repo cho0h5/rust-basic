b5962b30aa93afd8440ac9b44b988d626f423179
Youngho Cho <dominoyh5@gmail.com>
2020-07-17 12:31:35 +0900

    Learning triat

---
+++ 10-generic/trait_basic/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    pub trait Summarizable {
+        fn summary(&self) -> String;
+    }
+
+    pub struct NewsArticle {
+        pub headline: String,
+        pub location: String,
+        pub author: String,
+        pub content: String,
+    }
+
+    impl Summarizable for NewsArticle {
+        fn summary(&self) -> String {
+            format!("{}, by {} ({})", self.headline, self.author, self.location)
+        }
+    }
+
+    pub struct Tweet {
+        pub username: String,
+        pub content: String,
+        pub reply: bool,
+        pub retweet: bool,
+    }
+
+    impl Summarizable for Tweet {
+        fn summary(&self) -> String {
+            format!("{}: {}", self.username, self.content)
+        }
+    }
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of cou..."),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summary());
+}
