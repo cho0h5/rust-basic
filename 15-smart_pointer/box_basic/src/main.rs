dec0879e5cd549aa14bef363ed78ee2cf4a7c3d0
Youngho Cho <dominoyh5@gmail.com>
2020-07-22 16:46:41 +0900

    Learn Rc

--- 15-smart_pointer/box_basic/src/main.rs
+++ 15-smart_pointer/box_basic/src/main.rs
@@ -1,13 +1,14 @@
 use List::{Cons, Nil};
+use std::rc::Rc;
 
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 fn main() {
-    let list = Cons(1,
-        Box::new(Cons(2,
-            Box::new(Cons(3,
-                Box::new(Nil))))));
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
